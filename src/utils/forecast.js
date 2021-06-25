1c2460d77bd8a9a68547d6a309d96380c082f34a
13KuntalRoy <roykuntal1213@gmail.com>
2021-06-25 23:27:25 +0530

    update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,21 +1,18 @@
-const request =require('request')
+const request = require('request')
+
 const forecast=(latitude,longitude,callback)=>{
     const url="http://api.weatherstack.com/current?key=value&access_key=6bcbed6d12c2aa89ced647085624de30&query="+latitude+","+longitude+"&units=f"
-    request({url,json:true},(error,{body})=>{
-          if(error)
-     {
-          callback('unable to connect location service',undefined)
-     }
-     else if(body.error){
-           callback('unable to find Location',undefined); 
-     }
 
-     else{
-      callback(undefined,"current humidity"+body.current.humidity )
-
-    }
-    
-    
-})
+   request({url,json:true},(error,{body})=>{
+       if(error){
+           callback('Unable to connect location service',undefined)
+       }
+       else if(body.error){
+           callback('Unable to find location',undefined)
+       }
+       else{
+           callback(undefined,"current humidity:"+ body.current.humidity+",wind  direction:"+body.current.wind_dir+",Cloud Cover:"+body.current.cloudecover)
+       }
+   })
 }
 module.exports=forecast
